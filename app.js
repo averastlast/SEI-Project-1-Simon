29d5e0b87beb00825b7b265492099537c80c7260
averastlast <carapowpowell@gmail.com>
2019-04-09 14:55:55 -0400

    basics

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Hello World")
+
+
+document.getElementById("start").addEventListener("click", function() {
+    console.log("x")
+    document.querySelectorAll("input.submit")[1].classList.add("lightOn")})
