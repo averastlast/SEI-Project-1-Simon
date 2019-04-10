7bbb49e5a45a27515ab800815560aa8e9cb78f86
averastlast <carapowpowell@gmail.com>
2019-04-10 10:49:50 -0400

    blink works

--- app.js
+++ app.js
@@ -4,5 +4,25 @@ console.log("Hello World")
 document.querySelectorAll("input.start")[0].addEventListener("click", function() {
     console.log("x")
     document.querySelectorAll("input.yellow")[0].classList.add("lightOn")})
+    //remove
+    // document.querySelectorAll("input.yellow")[0].classList.remove("lightOn")
 
 let patternBulid = []
+
+//Blink Animation Test
+document.querySelectorAll("input.red")[0].addEventListener("click", function() {
+
+console.log("x")
+setTimeout(function(){ document.querySelectorAll("input.red")[0].classList.add("lightOn") }, 1000);
+setTimeout(function(){ document.querySelectorAll("input.red")[0].classList.remove("lightOn") }, 5000);
+
+
+})
+
+
+
+//Random Number Generator 
+console.log(randomNum(1,5))
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
