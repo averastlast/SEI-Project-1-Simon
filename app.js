3c80a5ce26d546b6e91471636d4684980a119ea5
averastlast <carapowpowell@gmail.com>
2019-04-12 15:03:34 -0400

    progress

--- app.js
+++ app.js
@@ -39,49 +39,121 @@ document.querySelectorAll("input.start")[0].addEventListener("click", function()
 })
 
 function playGame () {
+
 //COMPUTER CODE, TURN 1
+compSIMONrun()
 
-//SELECT RANDOM NUMBER (0-3)
-//     let randomNumDef = randomNum(0, 4)
-
-//PUSHES THAT RANDOM NUMBER INTO COMPUTER ARRAY
-//     computerSIMON.patternArray.push(randomNumDef)
-
-//LIGHT UP THAT DIV CORRESPONDING TO RANDOM NUMBER
-//     blinkDiv (randomNumDef) 
-
-//CHECK TEST: WORKING
-// console.log(computerSIMON.patternArray)
-
+ //maybe call function for turn?
+ if (computerSIMON.turnStatus === false) {
+     //run the user turn code
+     //at end of user code change back to computer code
+ 
 //USER CODE, TURN 1
 
 //MAKES DIVS CLICK AND LIGHT IN RESPONSE AND RECORDING THAT DIV CLICK. NEED SOUND FILE ADD
+//ALSO NESTED IF TO COMPARE, NEEDS TO HAPPEN IN EACH CLICK FOR UPDATE OF NESTED IF STATEMENT
 document.querySelectorAll("input.color")[0].addEventListener("click", function() {
     blinkbeepDiv (0)
     user.patternArray.push(0)
-    console.log(user.patternArray)
+    if (user.patternArray.length === computerSIMON.patternArray.length) {
+        comparePattern(user.patternArray, computerSIMON.patternArray)
+            if (comparePattern(user.patternArray, computerSIMON.patternArray)===true) {
+                compSIMONrun()
+                console.log("comp turn")
+// get back to computer turn not working
+            }
+            else {
+//END GAME
+                console.log("END GAME")
+            }
+    }
 })
 
 document.querySelectorAll("input.color")[1].addEventListener("click", function() {
     blinkbeepDiv (1)
     user.patternArray.push(1)
-    console.log(user.patternArray)
+    if (user.patternArray.length === computerSIMON.patternArray.length) {
+        comparePattern(user.patternArray, computerSIMON.patternArray)
+        if (comparePattern(user.patternArray, computerSIMON.patternArray)===true) {
+            compSIMONrun()
+            console.log("comp turn")
+// get back to computer turn not working
+        }
+        else {
+//END GAME
+            console.log("END GAME")
+        }
+
+    }
 })
 
 document.querySelectorAll("input.color")[2].addEventListener("click", function() {
     blinkbeepDiv (2)
     user.patternArray.push(2)
-    console.log(user.patternArray)
+    if (user.patternArray.length === computerSIMON.patternArray.length) {
+        comparePattern(user.patternArray, computerSIMON.patternArray)
+        if (comparePattern(user.patternArray, computerSIMON.patternArray)===true) {
+            compSIMONrun()
+            console.log("comp turn")
+// get back to computer turn not working
+        }
+        else {
+//END GAME
+        console.log("END GAME")
+        }}
+
+
 })
 
 document.querySelectorAll("input.color")[3].addEventListener("click", function() {
     blinkbeepDiv (3)
     user.patternArray.push(3)
-    console.log(user.patternArray)
+    if (user.patternArray.length === computerSIMON.patternArray.length) {
+        comparePattern(user.patternArray, computerSIMON.patternArray)
+        if (comparePattern(user.patternArray, computerSIMON.patternArray)===true) {
+            compSIMONrun()
+            console.log("comp turn")
+// get back to computer turn not working
+        }
+        else {
+//END GAME
+        console.log("END GAME")
+        }
+    }
 })
 
+}
 
 }
+
+
+function compSIMONrun() {
+    //SELECT RANDOM NUMBER (0-3)
+        let randomNumDef = randomNum(0, 4)
+    
+    //PUSHES THAT RANDOM NUMBER INTO COMPUTER ARRAY
+        computerSIMON.patternArray.push(randomNumDef)
+    
+    //LIGHT UP THAT DIV CORRESPONDING TO RANDOM NUMBER
+        setTimeout(function(){ blinkDiv (computerSIMON.patternArray[0]) }, 1000); 
+         if (computerSIMON.patternArray.length === 2) {
+            setTimeout(function(){ blinkDiv (computerSIMON.patternArray[1]) }, 2000);
+         }
+    
+    //CHECK TEST: WORKING
+        console.log(computerSIMON.patternArray)
+        // console.log(computerSIMON.turnStatus)
+    
+     //Change turn status
+        computerSIMON.turnStatus = false;
+        // console.log(computerSIMON.turnStatus)
+}
+
+
+
+
+
+
 
 
 //ATTEMPT TO SHORTEN CODE OF USER DIV CLICK, BREAKS WHEN RECORDING USER ARRAY BC OF CALL
@@ -99,7 +171,7 @@ function divclick (i) {
 
 
 
-//Gameplay Legnth, determines levels, taken from original game
+//Gameplay Legnth, determines levels, taken from original game ???????
 //SKILL LVL 1 SEQ 8
 //SKILL LVL 2 SEQ 14
 //SKILL LVL 3 SEQ 20
@@ -114,55 +186,30 @@ function patternPusher (seqLength) {
     return randomNumDef
 }
 
-//Comparing Arrays
+//Comparing Arrays - KEEP
 function comparePattern(x, y) {
-
     if (x.length != y.length) {
         return false;
     }
-
     for (let i = 0; i < x.length; i++) {
         if (x[i] !== y[i]) {
             return false;
         }
     }
-
     return true;
 }
 
-//Turn Cnger
-function turnChanger() {
-    if (!user.turnStatus) {
-        computerSIMON.turnStatus === true
-    }
-    else if (user.turnStatus) {
-        computerSIMON.turnStatus === false
-    }
-  }
-
-//When user clicks div[i] push i into userarray and does animation&sound??
-
-
-
-
-
-// function divclickRecord() {
-//    /* if () {
-//         user.patternArray.push(i)
-//     }*/
-// }
-
-//Singular Div Animation. i is div refers to div class Time needs to be parametertized?
+//Singular Div Animation. i is div refers to div class - KEEP
 function blinkDiv (i) {
     setTimeout(function(){ litUpDiv (i) }, 200);
-    setTimeout(function(){ dimmedDownDiv (i) }, 1000);
+    setTimeout(function(){ dimmedDownDiv (i) }, 500);
 }
 
-//Lights up divs
+//Lights up divs - KEEP
 function litUpDiv (i) {
     document.querySelectorAll("input.color")[i].classList.add("lightOn") 
 }
-//Dims down divs
+//Dims down divs - KEEP
 function dimmedDownDiv (i) {
     document.querySelectorAll("input.color")[i].classList.remove("lightOn") 
 }
@@ -185,10 +232,9 @@ function startButtonAnimation () {
     }
 }
 
-//Random Number Generator 
+//Random Number Generator - KEEP
 //Source: https://www.w3schools.com/js/js_random.asp
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
-
 //Pattern Array needs to be limited by level mode. Start with "Easy" array length 8.
